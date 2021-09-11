fbe7c8dae0b5ad5904992cb0abbfe1389499b29d
Marko Stojiljkovic <stojiljkovicmarko@yahoo.com>
2021-09-11 15:47:50 +0200

    uradjeno fetchovanje kandidata

---
+++ src/entities/Candidate.js
@@ -0,0 +1,8 @@
+export default class Candidate {
+    constructor(name, avatar, email, education) {
+        this.name = name;
+        this.avatar = avatar;
+        this.email = email;
+        this.education=education;
+    }
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
+
+import CandidateService from "../services/CandidateService";
+
 function Home() {
 
-    
+    const [ candidates, setCandidates ] = useState([]);
 
+
+    const loadCandidates = () => {
+        CandidateService.fetchAllCandidates().then(candidates => setCandidates(candidates));
+    }
+
+    useEffect(() => {
+        loadCandidates();
+    }, []);
+
+    console.log("form home: ", candidates);
+    
     return(
         <div className="bg-primary">
             HOME PAGE
+            
         </div>
     );
 }
