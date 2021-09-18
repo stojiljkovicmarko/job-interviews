4a86056abe1a3fbecc2ef37a9c726f11766ba8cc
Hehic3 <hehic3@gmail.com>
2021-09-18 11:19:31 +0200

    style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,20 @@ import {Link} from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+    const customStyle={
+        border: "1px solid rgb(194, 191, 191)",
+        padding: "5px 20px",
+        borderRadius: "5px",
+        color: "rgb(65, 64, 64)",
+    }
+
     return (
         <header>
                 <div className="navbar-fixed">
-                    <nav>
-                        <div className="nav-wrapper">
+                    <nav className="navbar navbar-light ">
+                        <div className="nav-wrapper container-fluid">
                             <h1>Interviews Reports </h1>
-                            <Link to="/">Candidates</Link>
+                            <Link style={customStyle} to="/">Candidates</Link>
                         </div>
                     </nav>
                 </div> 
