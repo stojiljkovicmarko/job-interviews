67f07013cfca1482149fea0279fe695b5e89ff06
Hehic3 <hehic3@gmail.com>
2021-09-18 11:54:27 +0200

    style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,6 @@ import {Link} from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
-    /*const customStyle={
-        border: "1px solid rgb(194, 191, 191)",
-        padding: "5px 20px",
-        borderRadius: "5px",
-        color: "rgb(65, 64, 64)",
-    }
-    */
 
     return (
         <header>
@@ -27,6 +20,7 @@ const Header = () => {
         </header>
     );
 };
+
 
 export default Header;
 
