470e8200daf704ca831d27caeae72f52c5e5fa6e
bogdan9291 <bogdan9291@gmail.com>
2021-09-18 09:20:48 +0200

    Added styling to candidate cards

--- src/components/CandidateCard/CandidateCard.js
+++ src/components/CandidateCard/CandidateCard.js
@@ -6,29 +6,28 @@ import "./CandidateCard.css";
 import { Link } from "react-router-dom";
 
 function CandidateCard(props) {
+  const { id, name, birthday, avatar, email, education } = props.candidate;
 
-    const { id, name, birthday, avatar, email, education } = props.candidate;
+  const generator = new AvatarGenerator();
 
-    const generator = new AvatarGenerator();
+  const avatarToDisplay = generator.generateRandomAvatar();
 
-    const avatarToDisplay = generator.generateRandomAvatar();
+  // zamotamo karticu u button tag
+  // const cardClickHandler = () => {
+  //     alert(name);
+  // }
 
-    // zamotamo karticu u button tag
-    // const cardClickHandler = () => {
-    //     alert(name);
-    // }
-
-    return (
-        
-            <div className="card col-3 pt-2 mb-2 text-decocation-none">
-                <Link to={"candidate-report/" + id} state={{name: name}}>
-                <img className="card-img" src={avatarToDisplay} alt="" />
-                <h4 className="card-text color-dark my-2"> {name} </h4>
-                <p className="text-muted"> {email} </p>
-                </Link>
-            </div>
-        
-    );
-};
+  return (
+    <div className="gutter">
+      <div className="card text-center scale pt-2 mb-2">
+        <Link to={"candidate-report/" + id} state={{ name: name }}>
+          <img className="card-img" src={avatarToDisplay} alt="" />
+          <h4 className="card-text color-dark my-2"> {name} </h4>
+          <p className="text-muted"> {email} </p>
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 export default CandidateCard;
--- src/components/CandidatePage/CandidatePage.js
+++ src/components/CandidatePage/CandidatePage.js
@@ -5,20 +5,25 @@ import CandidateCard from "../CandidateCard/CandidateCard";
 import "./CandidatePage.css";
 
 function CandidatePage({ candidates }) {
+  //console.log("from candidate page: ", candidates);
 
-    //console.log("from candidate page: ", candidates);
-
-    return (
-        <div className="container">
-            <div className="row">
-            {candidates.map(candidate => {
-                if (candidate.name) {
-                    return <CandidateCard className="card user-card" candidate={candidate} key={candidate.id} />
-                }
-            })}
-        </div>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="row">
+        {candidates.map((candidate) => {
+          if (candidate.name) {
+            return (
+              <CandidateCard
+                className="card user-card"
+                candidate={candidate}
+                key={candidate.id}
+              />
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default CandidatePage;
